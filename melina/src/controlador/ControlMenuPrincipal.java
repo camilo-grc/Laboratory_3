73abb37dc862186f94d9cbb27bc59e1f5f856010
kmilo <cmlgarcia0505@gmail.com>
2023-10-17 12:00:51 -0500

    adding laboratory folder (melina)

---
+++ melina/src/controlador/ControlMenuPrincipal.java
@@ -0,0 +1,50 @@
+/* Forest Suites es una app hecha en Java para la gestion de
+reservas en un hotel, con opciones de crear, modificar y buscar.
+
+Oscar Andres Hernandez Pineda - 2264488
+Camilo Andres Garcia - 2264484
+Alejandro Cuenca - 22644755
+
+Ult. fecha modificacion: 3/10/2023
+Version 1.0
+*/
+
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import vista.Acerca;
+import vista.Actualizar;
+import vista.Consultar;
+import vista.MenuPrincipal;
+import vista.Nuevo;
+
+public class ControlMenuPrincipal implements ActionListener{
+    
+    MenuPrincipal mm;
+    
+    public ControlMenuPrincipal(MenuPrincipal mm){
+        this.mm = mm;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(mm.btnReserva)) {
+            Nuevo mr = new Nuevo(mm);
+        }
+        if (e.getSource().equals(mm.btnConsultar)) {
+            Consultar mc = new Consultar(mm);
+        }
+        if (e.getSource().equals(mm.btnActualizar)) {
+            Actualizar mc = new Actualizar(mm);
+        }
+        if (e.getSource().equals(mm.btnAcerca)) {
+            Acerca ab = new Acerca(mm);
+        }
+    }
+}
